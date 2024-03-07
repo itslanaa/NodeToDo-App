596faea29ce23c3692750702e893ca76b6304312
Kaka Maulana Abdillah <maulanaabdillah73@gmail.com>
2024-03-07 21:38:24 +0700

    delete all contains files
    reset all inconivence.

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const taskController = require('../controllers/taskController');
-
-describe('Task Controller', () => {
-  it('should return an array of tasks', () => {
-    const tasks = taskController.getAllTasks();
-    assert.isArray(tasks);
-  });
-});
